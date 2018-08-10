243bd2517c6dcae774554305c35fcc0d18fb3d82
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-10 11:29:01 -0700

    add logic to talley up choices, add conditional states to show suggested choice on front end, add submit message container for each result

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,51 @@
-var ruby-rails = 0;
-var css-react = 0;
-var c-net = 0;
+var rubyRails = 0;
+var cssReact = 0;
+var cNet = 0;
 var none = 0;
 var all = 0;
+
+var tallyUpResults = function(choice) {
+  if ( choice === "ruby-rails") {
+    rubyRails += 1;
+  } else if ( choice === "css-react" ) {
+    cssReact += 1;
+  } else if ( choice === "c-net" ) {
+    cNet += 1;
+  } else if ( choice === "none") {
+    none += 1;
+  } else if ( choice  === "all") {
+    rubyRails += 1;
+    cssReact += 1;
+    cNet += 1;
+  }
+}
 
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     event.preventDefault();
     var choice1 = $("input:radio[name=question1]:checked").val();
+    tallyUpResults(choice1);
     var choice2 = $("input:radio[name=question2]:checked").val();
+    tallyUpResults(choice1);
     var choice3 = $("input:radio[name=question3]:checked").val();
+    tallyUpResults(choice1);
     var choice4 = $("input:radio[name=question4]:checked").val();
+    tallyUpResults(choice1);
     var choice5 = $("input:radio[name=question5]:checked").val();
+    tallyUpResults(choice1);
     var choice6 = $("input:radio[name=question6]:checked").val();
+    tallyUpResults(choice1);
 
-    var tallyUpResults = functions(choice) {
-      if ( choice === "ruby-rails") {
-
-      } else if ( choice === "css-react" ) {
-
-      } else if ( choice === "c-net" ) {
-
-      } else if ( choice === "all") {
-
-      } else if ( choice  === "none") {
-
-      }
+    if ( rubyRails === 6 ) {
+      $("ruby-rails").show();
+    } else if ( cssReact === 6 ) {
+      $("css-react").show();
+    } else if ( cNet === 6 ) {
+      $("c-net").show();
+    } else if ( none === 6 ) {
+      $("no-suggestion").show();
     }
+
+    console.log("ruby: " + rubyRails + " css-react: " + cssReact + " c-net " + cNet + " none: " + none + " all: " + all);
   });
 });
