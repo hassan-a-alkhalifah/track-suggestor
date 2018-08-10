c0a9005734e9f66ee2756e768eb33dea640df412
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-10 11:10:06 -0700

    change input varable names from question to choice, add initial conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
+var ruby-rails = 0;
+var css-react = 0;
+var c-net = 0;
+var none = 0;
+var all = 0;
+
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     event.preventDefault();
-    var question1 = $("input:radio[name=question1]:checked").val();
-    var question2 = $("input:radio[name=question2]:checked").val();
-    var question3 = $("input:radio[name=question2]:checked").val();
+    var choice1 = $("input:radio[name=question1]:checked").val();
+    var choice2 = $("input:radio[name=question2]:checked").val();
+    var choice3 = $("input:radio[name=question3]:checked").val();
+    var choice4 = $("input:radio[name=question4]:checked").val();
+    var choice5 = $("input:radio[name=question5]:checked").val();
+    var choice6 = $("input:radio[name=question6]:checked").val();
+
+    var tallyUpResults = functions(choice) {
+      if ( choice === "ruby-rails") {
+
+      } else if ( choice === "css-react" ) {
+
+      } else if ( choice === "c-net" ) {
+
+      } else if ( choice === "all") {
+
+      } else if ( choice  === "none") {
+
+      }
+    }
   });
 });
